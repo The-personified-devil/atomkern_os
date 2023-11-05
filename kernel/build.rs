c81cb6670aa8af134c323a0b5769cfd580ae998d
Leonhard Saam <leonhard.saam@yahoo.com>
2023-11-05 15:46:10 +0100

    chore: Update gitignores, kernel build improv
    Signed-off-by: Leonhard Saam <leonhard.saam@yahoo.com>

--- kernel/build.rs
+++ kernel/build.rs
@@ -10,7 +10,7 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let kernel_name = env::var("CARGO_PKG_NAME")?;
 
     // Tell rustc to pass the linker script to the linker.
-    println!("cargo:rustc-link-arg-bin={kernel_name}=--script=conf/linker.ld");
+    println!("cargo:rustc-link-arg=-Tconf/linker.ld");
 
     // Have cargo rerun this script if the linker script or CARGO_PKG_ENV changes.
     println!("cargo:rerun-if-changed=conf/linker.ld");
