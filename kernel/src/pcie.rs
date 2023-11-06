bdc998210222913bdf351d48aea184417708704a
Leonhard Saam <leonhard.saam@yahoo.com>
2023-11-06 23:41:27 +0100

    chore(xhci): Move xhci driver out of kernel
    Signed-off-by: Leonhard Saam <leonhard.saam@yahoo.com>

--- kernel/src/pcie.rs
+++ kernel/src/pcie.rs
@@ -1,13 +1,7 @@
 use crate::{mm::virt::MapFlags, phys_offset};
-#[allow(invalid_reference_casting)]
-use crate::{
-    println,
-    xhci::{msi_kekw, Config, HostCaps, OpRegs},
-};
-use arrayvec::ArrayVec;
-use bitflags::bitflags;
-use bitvec::prelude::*;
-use core::ptr::{addr_of, addr_of_mut, from_exposed_addr};
+
+use crate::println;
+
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 use proc_bitfield::*;
 use x86_64::{PhysAddr, VirtAddr};
@@ -63,18 +57,6 @@ pub struct CapabilityHeader {
 
 #[derive(Debug)]
 #[repr(C)]
-pub struct VirtioHeader {
-    header: CapabilityHeader,
-    length: u8,
-    cfg_type: u8,
-    bar: u8,
-    _res: [u8; 3],
-    offset: u32,
-    length_2: u32,
-}
-
-#[derive(Debug)]
-#[repr(C)]
 pub struct MsiCaps {
     pub header: CapabilityHeader,
     pub msg_control: MsiControl,
@@ -112,33 +94,6 @@ enum BarType {
     Bit32 = 0b00,
     Bit64 = 0b10,
 }
-
-fn get_bar(
-    allocator: &mut crate::frame::Allocator,
-    config: &VirtioConfig,
-    index: usize,
-    virt: u64,
-    offset: u64,
-) -> VirtAddr {
-    let bar = &config.bars[index as usize];
-    println!("bar {}", index);
-
-    let addr = match bar.bar_type().unwrap() {
-        BarType::Bit64 => {
-            ((config.bars[index as usize + 1].0 as u64) << 32) | ((bar.addr() as u64) << 4)
-        }
-        BarType::Bit32 => (bar.addr() as u64) << 4,
-    };
-    let addr = addr as u64 + offset as u64;
-
-    let virt = VirtAddr::new(virt);
-    crate::mm::virt::map_page(allocator, virt, x86_64::PhysAddr::new(addr / 4096 * 4096));
-    virt
-}
-
-// pub fn map_bar(bar: usize) {
-
-// }
 
 pub struct UserSpaceAddr {
     raw: u64,
@@ -219,8 +174,9 @@ static mut NET: Option<*mut VirtioConfig> = None;
 pub fn pcie_shenanigans(
     allocator: &mut crate::frame::Allocator,
     configs: &mut [crate::acpi::PcieRef],
-) -> ArrayVec<Device<'static>, 256> {
+) {
     let addr = configs[0].addr;
+
     let mut xhci = None;
     let mut net = None;
     for i in 0..256 {
@@ -239,163 +195,5 @@ pub fn pcie_shenanigans(
                 NET = Some(cfg_addr.as_mut_ptr::<VirtioConfig>());
             }
         }
-    }
-
-    let mut devs = ArrayVec::<Device<'static>, 256>::new();
-    return devs;
-
-    let xhci = xhci.unwrap();
-
-    // From here onward xhci only
-
-    println!("xhci: {:?}", xhci);
-    println!("bar0 {}, bar1 {}", xhci.bar0.addr(), xhci.bar1.0);
-
-    // Also is mmio registers base
-    let bar = match xhci.bar0.bar_type().unwrap() {
-        BarType::Bit64 => ((xhci.bar1.0 as u64) << 32) + ((xhci.bar0.addr() as u64) << 4),
-        BarType::Bit32 => (xhci.bar0.addr() as u64) << 4,
-    };
-
-    println!("bar64: {:?}", bar);
-
-    println!("fuck");
-    println!("addr: {:?}", (crate::phys_offset() + bar));
-
-    crate::mm::virt::map_page(
-        allocator,
-        crate::phys_offset() + bar,
-        x86_64::PhysAddr::new(bar / 4096 * 4096),
-    );
-
-    crate::mm::virt::map_page(
-        allocator,
-        crate::phys_offset() + bar + 4096usize,
-        x86_64::PhysAddr::new((bar + 4096) / 4096 * 4096),
-    );
-
-    let hostcaps_ptr = (crate::phys_offset() + bar).as_ptr::<HostCaps>();
-    let hostcaps = unsafe { &*(crate::phys_offset() + bar).as_ptr::<HostCaps>() };
-    println!("hostcaps: {:?}", hostcaps.clone());
-
-    let opregs = unsafe {
-        &mut *(crate::phys_offset() + bar + hostcaps.length as u64).as_mut_ptr::<OpRegs>()
-    };
-    unsafe {
-        addr_of_mut!(opregs.cmd).write_volatile(
-            addr_of_mut!(opregs.cmd)
-                .read_unaligned()
-                .clone()
-                .with_reset(true),
-        );
-    }
-
-    for i in 0..5 {
-        println!("ee");
-    }
-
-    // println!("opregs: {:?}", opregs.clone());
-    unsafe {
-        addr_of_mut!(opregs.config).write_volatile(
-            addr_of_mut!(opregs.config)
-                .read_unaligned()
-                .clone()
-                .with_max_slots(hostcaps.sparams1.max_slots()),
-        );
-    }
-    // println!("\nopregs: {:?}", opregs.clone());
-
-    let dev_ctx_arr = allocator.allocate().unwrap();
-    let cmd_queue = allocator.allocate().unwrap();
-    opregs.device_ctx_ptr = dev_ctx_arr.as_u64();
-
-    unsafe {
-        addr_of_mut!(opregs.cmd_ring).write_unaligned({
-            let mut a = addr_of_mut!(opregs.cmd_ring).read_unaligned();
-            a.set_ring_ptr(cmd_queue.as_u64());
-            a
-        });
-    }
-
-    let mut base = unsafe { (xhci as *const _) as *const CapabilityHeader };
-    let mut caps_ptr = unsafe { base.byte_offset(xhci.capabilities_ptr.into()) };
-
-    let mut msi = None;
-    loop {
-        let caps = unsafe { &*caps_ptr };
-        println!("caps: {:?}", caps);
-        if caps.id == 0x11 {
-            msi = Some(unsafe { &mut *caps_ptr.cast::<MsiCaps>().cast_mut() });
-        }
-        if caps.next == 0 {
-            break;
-        }
-        caps_ptr = unsafe { base.byte_offset(caps.next.into()) };
-    }
-
-    let msi = msi.unwrap();
-
-    assert_eq!(msi.table.bar(), 0);
-    assert_eq!(msi.pending.bar(), 0);
-
-    let msireg =
-        (crate::phys_offset() + bar + (msi.table.offset() << 3) as u64).as_mut_ptr::<MsiReg>();
-
-    let pba = (crate::phys_offset() + bar + (msi.pending.offset() << 3) as u64).as_mut_ptr::<u8>();
-
-    crate::mm::virt::map_page(
-        allocator,
-        x86_64::VirtAddr::new(msireg.addr() as u64),
-        x86_64::PhysAddr::new(bar + (msi.table.offset() << 3) as u64),
-    );
-
-    let msiregs =
-        unsafe { core::slice::from_raw_parts_mut(msireg, (msi.msg_control.size() + 1).into()) };
-
-    // let ptr = bitvec::ptr::BitPtr::from_mut(unsafe { &mut *pba });
-    // let pending =
-    //     unsafe { bitvec::slice::from_raw_parts_mut(ptr, (msi.msg_control.size() + 1).into()) };
-
-    // devs.push(Device {
-    //     msi: Some(Msi {
-    //         caps: msi,
-    //         table: msiregs,
-    //         pending: pending.unwrap(),
-    //     }),
-    // });
-
-    msi_kekw(allocator, &mut devs[0], bar, hostcaps_ptr, msi, opregs);
-}
-
-#[derive(Debug)]
-#[repr(C)]
-pub struct MsiReg {
-    pub addr: u64,
-    pub data: u32,
-    pub mask: u32,
-}
-
-struct Msi<'a> {
-    caps: &'a mut MsiCaps,
-    table: &'a mut [MsiReg],
-    pending: &'a mut BitSlice<u8, Lsb0>,
-}
-
-pub struct Device<'a> {
-    msi: Option<Msi<'a>>,
-}
-
-impl<'a> Device<'a> {
-    pub fn enable_msi(&mut self, id: u16) {
-        let msi = self.msi.as_mut().unwrap();
-        for (i, reg) in msi.table.iter_mut().enumerate() {
-            reg.addr = 0xFEE << 20;
-            reg.data = 20 + i as u32;
-            reg.mask = (i > 0).into();
-        }
-    }
-
-    pub fn enable_msi_all(&mut self) {
-        self.msi.as_mut().unwrap().caps.msg_control.set_enable(true);
     }
 }
